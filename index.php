0322efb4d76f5665ea4260efc0f8ec93fa3fdf55
DESKTOP-588KUEK\midix <midixas94@gmail.com>
2017-12-15 10:44:22 +0200

    mogo

---
+++ index.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>mogo</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
+<body>
+    <section  class="vienas">
+        <header>
+            <nav>
+                <h3>MoGo</h3>
+                <ul>
+                    <li>ABOUT</li>
+                    <li>SERVICE</li>
+                    <li>WORK</li>
+                    <li>BLOG</li>
+                    <li>CONTACT</li>
+                    <li><i class="fa fa-shopping-cart" style="font-size:18px"></i></li>
+                    <li><i class="fa fa-search" style="font-size:18px"></i></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="clearfix"></div>
+        <h2>Creative Template</h2>
+        <h1>WELCOME TO MOGO</h1>
+        <span></span>
+        <button>LEARN MORE</button>
+        <div class="clearfix"></div>
+        <ol class="one">
+            <li>INTRO</li>
+            <li>WORK</li>
+            <li>ABOUT</li>
+            <li>CONTACT</li>
+        </ol>
+    </section>
+    <section class="du">
+        <h2>What we do</h2>
+        <h1>STORY ABOUT US</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscinini elit, sed do eiusmod tempor incididun ut labore et magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamaco laboris nisi ut aliquip ex ea commodo consequat</p>
+        <div><img src="./img/what.png">
+        <img src="./img/we.png">
+        <img src="./img/do.png">
+        </div>
+        <span>
+            <ul>
+                <li><h4>42</h4>WEB DESIGN PROJECTS</li>
+                <li><h4>123</h4>HAPPY CLIENT</li>
+                <li><h4>15</h4>AWARD WINNER</li>
+                <li><h4>99</h4>CUP OF COFFEE</li>
+                <li><h4>24</h4>MEMBERS</li>
+            </ul>
+        </span>
+    </section>
+    <section class="trys">
+        <h1>We work with</h1>
+        <h2>AMAZING SERVICE</h2>
+        <div></div>
+        <ul>
+            <i class="fa fa-camera" style="font-size:31px"></i>
+            <li><h3>PHOTOGRAPHY</h3><p>tongue pork turducken pork chop buffalo. Strip steak ham hock</p></li>
+            <i class="fa fa-line-chart" style="font-size:31px"></i>
+            <li><h3>WEB DESIGN</h3><p>pancetta tail capicola sausage cupim buffalo short ribs salami jerky</p></li>
+            <i class="fa fa-desktop" style="font-size:31px"></i>
+            <li><h3>CREATIVITY</h3><p>Meatloaf rump pastrami frankfurter jerky meatball cow. Flank cow burgdoggen</p></li>
+        </ul>
+        <div></div>
+        <ul>
+            <i class="fa fa-file-text-o" style="font-size:31px"></i>
+            <li><h3>SEO</h3><p>Spicy jalapeno bacon ipsum dolor amet kielbasa corned beef pork chop rump</p></li>
+            <i class="fa fa-home" style="font-size:31px"></i>
+            <li><h3>CSS/HTML</h3><p>capicola beef ribs pork loin tail salami meatball chuck tenderloin beef</p></li>
+            <i class="fa fa-steam-square" style="font-size:31px"></i>
+            <li><h3>DIGITAL</h3><p> Landjaeger bacon jerky, shankle meatball short loin capicola spare ribs</p></li>
+        </ul>
+    </section>
+</body>
+</html>
